888631a4dd1eeb19da8c90b3af17899ee2c96deb
borsodiadam <borsodi.dm@gmail.com>
2024-05-09 08:24:40 +0200

    Translation

--- emails/Welcome.tsx
+++ emails/Welcome.tsx
@@ -1,7 +1,8 @@
-import { Body, Container, Head, Html, Preview, Link } from '@react-email/components';
+import { Body, Container, Head, Html, Preview } from '@react-email/components';
 import * as React from 'react';
 import WelcomeHun from './WelcomeHun';
 import { main, container } from './Welcome.styles';
+import WelcomeEng from './WelcomeEng';
 
 interface WelcomeProps {
    name: string;
@@ -13,7 +14,9 @@ const Welcome = ({ name = 'adam', lang = 'en' }: WelcomeProps) => (
       <Head />
       <Preview>{lang === 'hu' ? 'A pénz a kitartás fáján nő.' : 'Money grows on the tree of persistence'}</Preview>
       <Body style={main}>
-         <Container style={container}>{lang == 'hu' && <WelcomeHun name={name} />}</Container>
+         <Container style={container}>
+            {lang == 'hu' ? <WelcomeHun name={name} /> : <WelcomeEng name={name} />}
+         </Container>
       </Body>
    </Html>
 );
