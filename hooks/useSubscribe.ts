1372e6215f8ff68e600e1fcbd63546879e153744
borsodiadam <borsodi.dm@gmail.com>
2024-05-09 08:52:02 +0200

    Subscribe translation

--- hooks/useSubscribe.ts
+++ hooks/useSubscribe.ts
@@ -1,21 +1,23 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { subscribe as subscribeFn } from '@/services/apiSubscribe';
 import { toast } from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
 
 export const useCreateSubscription = () => {
    const queryClient = useQueryClient();
+   const { t } = useTranslation();
+   const successMessage = t('Sikeresen feliratkozál! 🤩');
+   const errorMessage = t('Valami hiba történt! 🤯');
    const { mutate: createSubscription, isPending: isCreating } = useMutation({
       mutationFn: subscribeFn,
 
       onSuccess: () => {
-         toast.success('Sikeresen feliratkozál! 🤩');
+         toast.success(successMessage);
          queryClient.invalidateQueries({ queryKey: ['subscription'] });
       },
 
-      onError: err => toast.error(err.message || 'Valami hiba történt! 🤯'),
+      onError: err => toast.error(err.message || errorMessage),
    });
-
-   //TODO TRANSLATE THIS
 
    return { createSubscription, isCreating };
 };
--- services/apiSubscribe.ts
+++ services/apiSubscribe.ts
@@ -1,14 +1,21 @@
 import { supabase } from '@/utils/supabase';
+import { getCookie } from 'cookies-next';
 import { FieldValues } from 'react-hook-form';
 
 export const subscribe = async (subscription: FieldValues) => {
    const { data, error } = await supabase.from('subscriptions').insert(subscription).select();
+   const cookie = getCookie('i18next');
+   let message_23505;
+
+   if (cookie === 'hu') {
+      message_23505 = 'Korábban már feliratkozál!';
+   } else if (cookie === 'en') {
+      message_23505 = 'You already subscribed!';
+   }
 
    if (error) {
-      console.log(error);
-      if (+error.code === 23505) throw new Error('Korábban már feliratkozál!');
+      if (+error.code === 23505) throw new Error(message_23505);
    }
-   console.log(data);
 
    return data;
 };
