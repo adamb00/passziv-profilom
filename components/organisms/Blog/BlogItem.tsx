e81cd3c387d40704a220764aa796113626cb8baf
borsodiadam <borsodi.dm@gmail.com>
2024-05-12 19:49:51 +0200

    Blog

--- components/organisms/Blog/BlogItem.tsx
+++ components/organisms/Blog/BlogItem.tsx
@@ -4,6 +4,8 @@ import { getCookie } from 'cookies-next';
 import React from 'react';
 import { FacebookIcon, FacebookMessengerIcon, XIcon } from 'react-share';
 import { FacebookShareButton, FacebookMessengerShareButton, TwitterShareButton } from 'react-share';
+import { IoIosLink } from 'react-icons/io';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export default function BlogItem({ blog }: { blog: IBlogPost }) {
    const { id, title, blog_post, created_at } = blog;
@@ -17,12 +19,15 @@ export default function BlogItem({ blog }: { blog: IBlogPost }) {
             <FacebookShareButton url={url}>
                <FacebookIcon className='blog__share--icon' round />
             </FacebookShareButton>
-            <FacebookMessengerShareButton url={url} appId={process.env.NEXT_PUBLIC_FACEBOOK_APP_ID}>
+            {/* <FacebookMessengerShareButton url={url} appId={process.env.NEXT_PUBLIC_FACEBOOK_APP_ID}>
                <FacebookMessengerIcon className='blog__share--icon' round />
-            </FacebookMessengerShareButton>
+            </FacebookMessengerShareButton> */}
             <TwitterShareButton url={url}>
                <XIcon className='blog__share--icon' round />
             </TwitterShareButton>
+            <CopyToClipboard text={url}>
+               <IoIosLink className='blog__share--icon blog__share--icon__share' />
+            </CopyToClipboard>
          </div>
          <div className='blog__post' dangerouslySetInnerHTML={{ __html: blog_post }}></div>
          <div className='blog__created'>{formatDate(created_at.toString(), lang ?? 'hu')}</div>
