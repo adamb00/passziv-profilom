c6a0f96b15cfd48bc45aebdc190b3caf6498fdab
borsodiadam <borsodi.dm@gmail.com>
2024-06-05 08:37:36 +0200

    tlc

--- pages/tlc-trading.tsx
+++ pages/tlc-trading.tsx
@@ -1,9 +1,18 @@
+import { getCookie } from 'cookies-next';
+import { cookies } from 'next/headers';
 import Link from 'next/link';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function TLCtrading() {
    const { t } = useTranslation();
+   const cookie = getCookie('i18next') as string;
+   const lang = cookie && cookie.toLocaleUpperCase();
+   const video =
+      cookie === 'hu'
+         ? `https://fbfythyraexrdhlkdana.supabase.co/storage/v1/object/public/videos/TLC.mp4?t=2024-05-07T06%3A34%3A02.330Z`
+         : `https://fbfythyraexrdhlkdana.supabase.co/storage/v1/object/public/videos/TLC_EN.mp4`;
+
    return (
       <div className='project'>
          <Link href='https://app.tlctrading.ai/register/galamb65' target='_blank'>
@@ -43,12 +52,90 @@ export default function TLCtrading() {
 
          <div className='project__video'>
             <video className='project__video--content' controls preload='preload' id='tlc'>
-               <source
-                  src='https://fbfythyraexrdhlkdana.supabase.co/storage/v1/object/public/videos/TLC.mp4?t=2024-05-07T06%3A34%3A02.330Z'
-                  type='video/mp4'
-               />
+               <source src={video} type='video/mp4' />
             </video>
          </div>
+
+         <div>
+            <h2 className='heading-secondary heading-secondary--sub'>{t('Mi lehet ennél jobb és egyszerűbb ??')}</h2>
+
+            <p className='paragraph paragraph__alt'>
+               {t(
+                  'Az egyik legsikeresebb, automatizált kereskedés. December óta figyeljük, de most már muszáj volt beszállnunk annyi jót hallottunk róla és hatalmas projecté nőtte ki magát, ígéretes tervekkel.'
+               )}
+            </p>
+            <p className='paragraph paragraph__alt'>{t('Ha még nem vagy benne, nem késő!')}</p>
+            <p className='paragraph paragraph__alt'>
+               {t(
+                  'Szerintünk ez egy igazán hosszútávú, fent tartható és nem utolsó sorban kormány által támogatott project!'
+               )}
+            </p>
+            <Link className='link' target='_blank' href='https://app.tlctrading.ai/register/galamb65'>
+               <p className='paragraph paragraph_alt important'>{t('15%-30% közötti havi hozam')}</p>
+            </Link>
+         </div>
+
+         <picture className='project__picture'>
+            <source
+               srcSet={
+                  cookie === 'hu'
+                     ? `/tlc-boss.png 320w, /tlc-boss.png 620w, /tlc-boss.png 1980w`
+                     : `/tlc-boss-en.png 320w, /tlc-boss-en.png 620w, /tlc-boss-en.png 1980w`
+               }
+            />
+            <img
+               loading='lazy'
+               src={`/tlc-boss${cookie == 'en' ? '-en' : ''}.png`}
+               alt='TLC trading ai'
+               className='project__image'
+            />
+         </picture>
+
+         <p className='paragraph paragraph__alt'>{t('Csináljuk meg együtt, legyünk passzívan szabadok‼️')}</p>
+
+         <p className='paragraph'>
+            <Link
+               className='link'
+               target='_blank'
+               href='https://drive.google.com/drive/u/2/folders/10oI6tmZCrWQ-Us3Qy5E8bfvAD3uRRFfI'
+            >
+               {t('Fontos tudnivalók és hasznos videók')}
+            </Link>
+         </p>
+
+         <Link href='https://app.tlctrading.ai/register/galamb65' target='_blank'>
+            <button className='btn btn--animated btn--green'>{t('Kezdjük el most!')}</button>
+         </Link>
+
+         <picture className='project__picture'>
+            <source
+               srcSet={
+                  cookie === 'hu'
+                     ? `/tlc-details.png 320w, /tlc-details.png 620w, /tlc-details.png 1980w`
+                     : `/tlc-details-en.png 320w, /tlc-details-en.png 620w, /tlc-details-en.png 1980w`
+               }
+            />
+            <img
+               loading='lazy'
+               src={`/tlc-details${cookie == 'en' ? '-en' : ''}.png`}
+               alt='TLC trading ai'
+               className='project__image'
+            />
+         </picture>
+
+         <p className='paragraph'>
+            <Link
+               className='link link--green'
+               target='_blank'
+               href={`https://cdn.tlctrading.ai/presentations%2FAffiliate_${lang}.pdf`}
+            >
+               {t('Hivatalos prezentáció')}
+            </Link>
+         </p>
+
+         <Link href='https://app.tlctrading.ai/register/galamb65' target='_blank'>
+            <button className='btn btn--animated btn--orange'>{t('Ne hagyd ki az esélyt!')}</button>
+         </Link>
       </div>
    );
 }
